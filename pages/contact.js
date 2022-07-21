d1a0814ed9972f3d11a3bc06ea8cc5959a1a088e
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-07-20 22:45:53 -0700

    &apos; fix

--- pages/contact.js
+++ pages/contact.js
@@ -7,14 +7,14 @@ export default function ContactPage() {
   return (
     <Layout title="Contact">
       <Animated>
-        <Hero title="Let's start making your perfect website" />
+        <Hero title="Let's create something they'll love" />
         <Section>
           <div className="p-2">
             <p className="text-center">
               With over 2 years of experience creating User Experience focused
-              websites, I’m able to tailor websites to suit your companies
-              needs. Business and techonology are always evolving, you'll need
-              innovative solutions to stay ahead of the curve. Your website
+              websites, I&apos;m able to tailor websites to suit your companies
+              needs. Business and techonology are always evolving, you&apos;ll
+              need innovative solutions to stay ahead of the curve. Your website
               should engage your customers effectively and make your job easier.
               Good tech creates happy customers. <br /> <br />
               <strong>Just shoot me an email</strong>, and we&apos;ll set up a
